5ade432d29f73b8ff76ee3bc549868782f13a60e
Naga Vijayapuram <naga.vijayapuram.ap@nielsen.com>
2016-08-26 19:16:49 -0400

    complete

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # run-spark-jobserver
 Quickly Setup and Run Spark Job Server - https://github.com/spark-jobserver/spark-jobserver
 
+Steps -
+
+* Clone this repo -
+
+git clone https://github.com/nvijayap/run-spark-jobserver.git
+
+* Run spark-jobserver -
+
+run-spark-jobserver/run.sh
+
+* Verify by visiting http://localhost:8090
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TOP=$(cd `dirname $0`; pwd -P)
+
 if [ $# -ne 2 ]; then
   echo -e "\nNeed these args -"
   echo -e "\n1. Fullpath of SPARK_HOME"
@@ -7,28 +9,22 @@ if [ $# -ne 2 ]; then
   echo ; exit 1
 fi
 
-[ -d spark-jobserver ] || git clone https://github.com/spark-jobserver/spark-jobserver
-
-if [ ! -d spark-jobserver/target ]; then
-  cd spark-jobserver ; sbt assembly
-fi
+[ -d $TOP/spark-jobserver ] || git clone https://github.com/spark-jobserver/spark-jobserver
+[ -d $TOP/spark-jobserver/target ] || ( cd $TOPspark-jobserver ; sbt assembly )
 
 mkdir -p try
+/bin/cp -p $TOP/spark-jobserver/bin/server_start.sh $TOP/try
+/bin/cp -p $TOP/spark-jobserver/bin/setenv.sh $TOP/try
+/bin/cp -p $TOP/spark-jobserver/config/local.conf.template $TOP/try/local.conf
+/bin/cp -p $TOP/spark-jobserver/config/local.sh.template $TOP/try/settings.sh
+/bin/cp -p `ls $TOP/spark-jobserver/target/scala-2.1?/root-assembly-0.7.0-SNAPSHOT.jar` $TOP/try/spark-job-server.jar 
+/bin/cp -p $TOP/spark-jobserver/job-server/config/log4j-server.properties $TOP/try
 
-/bin/cp -p spark-jobserver/bin/server_start.sh try
-/bin/cp -p spark-jobserver/bin/setenv.sh try
-/bin/cp -p spark-jobserver/config/local.conf.template try/local.conf
-/bin/cp -p spark-jobserver/config/local.sh.template try/settings.sh
-/bin/cp -p `ls spark-jobserver/target/scala-2.1?/root-assembly-0.7.0-SNAPSHOT.jar` try/spark-job-server.jar 
-/bin/cp -p spark-jobserver/job-server/config/log4j-server.properties try
+echo "#----------------------------" >> $TOP/try/settings.sh
+echo "SPARK_HOME=$1" >> $TOP/try/settings.sh
+echo "SPARK_CONF_DIR=$1/conf" >> $TOP/try/settings.sh
 
-echo "#----------------------------" >> try/settings.sh
-echo "SPARK_HOME=$1" >> try/settings.sh
-echo "SPARK_CONF_DIR=$1/conf" >> try/settings.sh
-
-cmd=try/server_start.sh
-
+cmd=$TOP/try/server_start.sh
 [ "$2" == "fg" ] && cmd="JOBSERVER_FG=1 $cmd"
-
 eval $cmd
 
