258f0ac6c699c7d27fd7247ff6cdc005be120f24
Naga Vijayapuram <naga.vijayapuram.ap@nielsen.com>
2016-08-26 21:44:26 -0400

    handling rest port and jmx port

--- run.sh
+++ run.sh
@@ -9,8 +9,13 @@ if [ $# -ne 2 ]; then
   echo ; exit 1
 fi
 
-[ -d $TOP/spark-jobserver ] || git clone https://github.com/spark-jobserver/spark-jobserver $TOP/spark-jobserver
-[ -d $TOP/spark-jobserver/target ] || ( cd $TOP/spark-jobserver ; sbt assembly )
+if [ -d $TOP/spark-jobserver ]; then
+  ( cd $TOP/spark-jobserver ; git pull )
+else
+  git clone https://github.com/spark-jobserver/spark-jobserver $TOP/spark-jobserver
+fi
+
+( cd $TOP/spark-jobserver ; sbt assembly )
 
 INSTALL_DIR=$TOP/try ; mkdir -p $INSTALL_DIR
 /bin/cp -p $TOP/spark-jobserver/bin/server_start.sh $TOP/try
@@ -25,6 +30,22 @@ echo "INSTALL_DIR=$INSTALL_DIR" >> $INSTALL_DIR/settings.sh
 echo "LOG_DIR=$INSTALL_DIR/logs" >> $INSTALL_DIR/settings.sh
 echo "SPARK_HOME=$1" >> $INSTALL_DIR/settings.sh
 echo "SPARK_CONF_DIR=$1/conf" >> $INSTALL_DIR/settings.sh
+
+for port in `seq 8090 8099`; do
+  nc -z localhost $port >/dev/null 2>&1
+  [ $? -ne 0 ] && REST_PORT=$port && break
+done
+
+sed "s|port = 8090|port = $REST_PORT|" $INSTALL_DIR/local.conf >| $INSTALL_DIR/new_local.conf
+/bin/mv $INSTALL_DIR/new_local.conf $INSTALL_DIR/local.conf
+
+for port in `seq 9990 9999`; do
+  nc -z localhost $port >/dev/null 2>&1
+  [ $? -ne 0 ] && JMX_PORT=$port && break
+done
+
+sed "s|JMX_PORT=9999|JMX_PORT=$JMX_PORT|" $INSTALL_DIR/setenv.sh >| $INSTALL_DIR/new_setenv.sh
+/bin/mv $INSTALL_DIR/new_setenv.sh $INSTALL_DIR/setenv.sh
 
 cmd=$TOP/try/server_start.sh
 [ "$2" == "fg" ] && cmd="JOBSERVER_FG=1 $cmd"
