88e79c12752a8b05f94e021c0cc9b0109882c1d4
Naga Vijayapuram <naga.vijayapuram.ap@nielsen.com>
2016-08-26 20:18:18 -0400

    using INSTALL_DIR/logs as the logs dir

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ fi
 [ -d $TOP/spark-jobserver ] || git clone https://github.com/spark-jobserver/spark-jobserver $TOP/spark-jobserver
 [ -d $TOP/spark-jobserver/target ] || ( cd $TOP/spark-jobserver ; sbt assembly )
 
-mkdir -p $TOP/try
+INSTALL_DIR=$TOP/try ; mkdir -p $INSTALL_DIR
 /bin/cp -p $TOP/spark-jobserver/bin/server_start.sh $TOP/try
 /bin/cp -p $TOP/spark-jobserver/bin/setenv.sh $TOP/try
 /bin/cp -p $TOP/spark-jobserver/config/local.conf.template $TOP/try/local.conf
@@ -20,9 +20,11 @@ mkdir -p $TOP/try
 /bin/cp -p `ls $TOP/spark-jobserver/target/scala-2.1?/root-assembly-0.7.0-SNAPSHOT.jar` $TOP/try/spark-job-server.jar 
 /bin/cp -p $TOP/spark-jobserver/job-server/config/log4j-server.properties $TOP/try
 
-echo "#----------------------------" >> $TOP/try/settings.sh
-echo "SPARK_HOME=$1" >> $TOP/try/settings.sh
-echo "SPARK_CONF_DIR=$1/conf" >> $TOP/try/settings.sh
+echo -e "\n#-------------------------------------------\n" >> $INSTALL_DIR/settings.sh
+echo "INSTALL_DIR=$INSTALL_DIR" >> $INSTALL_DIR/settings.sh
+echo "LOG_DIR=$INSTALL_DIR/logs" >> $INSTALL_DIR/settings.sh
+echo "SPARK_HOME=$1" >> $INSTALL_DIR/settings.sh
+echo "SPARK_CONF_DIR=$1/conf" >> $INSTALL_DIR/settings.sh
 
 cmd=$TOP/try/server_start.sh
 [ "$2" == "fg" ] && cmd="JOBSERVER_FG=1 $cmd"
